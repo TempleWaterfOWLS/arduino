97a4e34e649ba18a218416f5f2dc53ad07378145
Jake S <tud45372@temple.edu>
2014-10-03 16:55:57 -0400

    Zack did some stupid bullshit and now jake wants his computer back

--- WebServer/WebServer/WebServer.ino
+++ WebServer/WebServer/WebServer.ino
@@ -11,18 +11,18 @@
 #include <avr/pgmspace.h>
 #include <SPI.h>
 #include <Ethernet.h>
-#include <String.h>
+//#include <String.h>
 #include <ros.h>
 #include <std_msgs/String.h>
 
 //ROS defines
-ros::NodeHandle nh;
-std_msgs::String str_msg;
-ros::Publisher motor_response("motor_response", &str_msg);
+//ros::NodeHandle nh;
+//std_msgs::String str_msg;
+//ros::Publisher motor_response("motor_response", &str_msg);
 
 
 //setup ros node and publisher
-ros::Publisher pub_humidity("humidity_sensor", &humidity_msg);
+//ros::Publisher pub_humidity("humidity_sensor", &humidity_msg);
 //VRCSR protocol defines
 const char SYNC_REQUEST[] = {0xF5, 0x5F};
 const char SYNC_RESPONSE[] = {0x0F,0xF0};
@@ -67,9 +67,8 @@ EthernetServer server(80);
 long T_CUR = millis();
 long T_HTTP = millis();
 long ELAPSED;
-
+String motor_response = String();
 String motor_speed = String(10);
-
 float thrust[2] = {0.0,0.0};
 char node_id = 100;
 boolean excelsior_lyfe = false;
@@ -303,24 +302,24 @@ void update_motors(void)
 void get_motor_condition(){
   char node_id[]={0,1};
   char response;
+  char motor_array[100];
   int offset = 0;
+  
   //get information from motor controller node 0
   set_motors_thrust(node_id[0],thrust,sizeof(thrust));
   // Consider clearing global buffer. Could replace 0 with a string to show non-atomicity
-  memset(&motorResponseStorage[0],0, sizeof(motorResponseStorage));
+  //memset(&motorResponseStorage[0],0, sizeof(motorResponseStorage));
   delay(100);
   if (Serial3.available()){
     Serial.println("Data from motorC node 0");
     while (Serial3.available()>0){
         response= Serial3.read();	//read Serial        
-        Serial.println(response, HEX); 
-        // Write response to global storage variable
-        motorResponseStorage[offset] = response;
-        offset++; 
+        Serial.print(response, HEX);
+        
     }
   }
-  Serial.println("New stored array is: ");
-  Serial.print(motorResponseStorage);
+  
+  
   //get information from motor controller node 1
   set_motors_thrust(node_id[1],thrust,sizeof(thrust));
   delay(100);
@@ -328,9 +327,22 @@ void get_motor_condition(){
     Serial.println("Data from motorC node 1");
     while (Serial3.available()>0){
         response= Serial3.read();	//read Serial        
-        Serial.println(response, HEX); 
+        Serial.println(response, HEX);
+        // Write response to global storage variable
+        motor_response.concat(response);
+        //motor_array[offset] = response;
+        //offset++;  
     }
   }
+  Serial.println("New stored array is: ");
+  int ite;
+  for (ite = 0; ite<motor_response.length();ite++)
+  {
+  Serial.print(motor_response[ite],HEX);
+  Serial.print('-');
+  }
+  Serial.print("\n");
+  motor_response = "";
 }
 
 void setup()
@@ -346,9 +358,9 @@ void setup()
   Serial.println(Ethernet.localIP());
   
   //begin ros node
-  nh.initNode();
+//  nh.initNode();
   //begin ros topic
-  nh.advertise(motor_response);
+//  nh.advertise(motor_response);
 }
 
 void loop() {
@@ -363,9 +375,9 @@ void loop() {
   }*/
   update_motors();
   get_motor_condition();
-  str_msg.data="hello";
+/*  str_msg.data="hello";
   motor_response.publish(&str_msg);
-  nh.spinOnce();
+  nh.spinOnce(); */
   // give the web browser time to receive the data
   delay(1000);
   // close the connection:
